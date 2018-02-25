425ae15741bd2257f03a1a5add73ad542c8d49ac
David CHEN Zhuo <david.chen.zhuo@gmail.com>
2018-02-26 03:19:48 +0800

    Test commit author name.

--- src/test/java/com/example/demo/SpringDemoApplicationTest.java
+++ src/test/java/com/example/demo/SpringDemoApplicationTest.java
@@ -10,7 +10,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 public class SpringDemoApplicationTest {
     
     @Test
-    public void contextLoads() {
+    public void test1() {
     }
     
 }
